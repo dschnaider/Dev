bedddf9de0c18ccefd978fa05d0ceaf7dca46468
dschnaider <dschnaider@gmail.com>
2013-10-16 17:43:01 -0300

    First test

---
+++ RTest/HelloWorld.R
@@ -0,0 +1,8 @@
+# TODO: Add comment
+# 
+# Author: danis
+###############################################################################
+
+
+a <- c(1,2,3,4,5)
+print(paste("Hello world + vector a = ",a))
